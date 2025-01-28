5497e913fc73b2089c4d08d4d9261b58a23e4e62
PieterNatanael <pieternatanael@icloud.com>
2025-01-28 08:50:52 +0800

    Initial Commit

---
+++ SwiftLibrary/SwiftLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLibraryApp.swift
+//  SwiftLibrary
+//
+//  Created by Pieter Yoshua Natanael on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftLibraryTests/SwiftLibraryTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLibraryTests.swift
+//  SwiftLibraryTests
+//
+//  Created by Pieter Yoshua Natanael on 28/01/25.
+//
+
+import Testing
+@testable import SwiftLibrary
+
+struct SwiftLibraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
